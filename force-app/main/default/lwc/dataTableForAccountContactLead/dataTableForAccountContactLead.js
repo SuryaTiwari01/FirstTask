b25b041e45549de98022d05f35bdb8a4e0b5f8a0
suryatiwari01 <suryatiwari01@gmail.com>
2021-10-11 01:26:35 +0530

    Pushed code after optimizing

--- force-app/main/default/lwc/dataTableForAccountContactLead/dataTableForAccountContactLead.js
+++ force-app/main/default/lwc/dataTableForAccountContactLead/dataTableForAccountContactLead.js
@@ -1,100 +1,52 @@
-import { LightningElement ,api, wire, track} from 'lwc';
-import getAccountList from '@salesforce/apex/AccountHelper.getAccountList';
-import getContactList from '@salesforce/apex/AccountHelper.getContactList';
-import getLeadList from '@salesforce/apex/AccountHelper.getLeadList';
-export default class dataTableForAccountContactLead extends LightningElement {
-    
-    @track columns = [
-        {label: 'Account name', fieldName: 'Name', type: 'text', sortable: true},
-        {label: 'Type', fieldName: 'Type', type: 'text', sortable: true},
-        {label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'Currency', sortable: true},
-        {label: 'Phone', fieldName: 'Phone', type: 'phone', sortable: true},
-    ];
-    
-    @track columns1 = [
-        {label: 'Contact Name', fieldName: 'Name', type: 'text', sortable: true},
-        {label: 'Title', fieldName: 'Title', type: 'text(128)', sortable: true},
-        {label: 'Email', fieldName: 'Email', type: 'Email', sortable: true},
-        {label: 'Phone', fieldName: 'Phone', type: 'phone', sortable: true}
-    ];
-    
-    @track columns2 = [
-        {label: 'Lead name', fieldName: 'Name', type: 'text', sortable: true},
-        {label: 'Company', fieldName: 'Company', type: 'Text(255)', sortable: true},
-        {label: 'Lead Status', fieldName: 'Status', type: 'Picklist', sortable: true},
-        {label: 'Phone', fieldName: 'Phone', type: 'phone', sortable: true},
-    ];
+import { LightningElement, api} from 'lwc';
+import getDataList from '@salesforce/apex/AccountHelper.getDataList';
+import { NavigationMixin } from 'lightning/navigation';
 
-    //@track selectedObjectName;
+const ACTIONS = [{label:'Delete', name:'delete'}]
+
+
+export default class dataTableForAccountContactLead extends NavigationMixin(LightningElement) {
     
-    @track accountSelected;
-    @track contactSelected;
-    @track leadSelected;
-    @track error;
-    @track accList;
-    @track conList;
-    @track ldList;
+    selectedObject;    
+    error;
+    dataList;
+    
+    columns = [
+        {label: 'Name', fieldName: 'name', type: 'text',  sortable: true},
+        {label: 'Id', fieldName: 'dataId', type: 'text', sortable: true},
+        {label: 'Phone', fieldName: 'phone', type: 'text', sortable: true}
+    ];
+ 
 
     selectionChangeHandler(event) {
-        console.log('Check function Call');
-		var selectedObject = event.target.value;
+        var selectedObject = event.target.value;
 
-        this.selectedObjectName=selectedObject;
-        if(selectedObject== 'Account'){
-            this.accountSelection(selectedObject);
-            
-        
-        }else if(selectedObject == 'Contact'){
-            this.contactSelection(selectedObject);
-        
-        }else if(selectedObject == 'Lead'){
-            this.leadSelection(selectedObject);
-        } else {
-            this.accountSelected=false;
-            this.contactSelected=false;
-            this.leadSelected=false;
-        }
-	}
-    leadSelection(selectedObject){
-        this.leadSelected=true;
-        console.log('Lead--- selectedObject --- ', selectedObject );
-        this.accountSelected=false;
-        this.contactSelected=false;
+        this.selectedObject = selectedObject;
+        this.selectedObjectName = selectedObject;
 
-        console.log('Lead--- selectedObject --- ', selectedObject );
-        getLeadList()
-        .then(result =>{ this.ldList = result;
+        getDataList({ 
+            selectedObject : this.selectedObject
         })
-        .catch(error =>{
-            this.error = error;
-        })
-    }
-    accountSelection(selectedObject){
-        this.accountSelected=true;
-        this.contactSelected=false;
-        this.leadSelected=false;
-        console.log('Account--- selectedObject --- ', selectedObject );
-        getAccountList()
-        .then(result =>{
-            this.accList = result;
-        })
-        .catch(error =>{
-            this.error = error;
-        })
-        
-    }
-    contactSelection(selectedObject){
-        this.accountSelected=false;
-        this.contactSelected=true;
-        this.leadSelected=false;
 
-        console.log('Contact--- selectedObject --- ', selectedObject );
-        getContactList()
-        .then(result =>{ this.conList = result;
+        .then(result => {
+            this.dataList = result;
         })
-        .catch(error =>{
+ 
+        .catch(error => {
             this.error = error;
-        })
+        });
     }
+
+    
+    navigateToNewRecordPage(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: this.selectedObject,
+                actionName: 'new'
+            }
+        })
+    } 
+
 
 }
